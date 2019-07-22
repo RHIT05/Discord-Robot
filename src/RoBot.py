e5974bfb4d4cb47f665fe1b87aa833cf395a925e
Bailey Morgan <bailey.mccarty.morgan@gmail.com>
2019-07-21 22:01:16 -0400

    invoke commands for bot

--- src/RoBot.py
+++ src/RoBot.py
@@ -48,8 +48,7 @@ async def on_member_remove(member):
 @client.event
 async def on_message(message):
     context = await client.get_context(message)
-    if message.author.bot:
-        await client.invoke(context)
+    await client.invoke(context)
 
 
 @client.event
